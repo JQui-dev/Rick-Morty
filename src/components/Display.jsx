99db8cb59e64acf0d9787d7eb6b4ada2635120d5
Q1one <jqui.dev@gmail.com>
2023-05-04 22:16:12 -0500

    new display

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -6,17 +6,17 @@ function Display({data, error}) {
             {
                 error!="" ? <h1 className="error">{error}</h1> :
                 data.map(res => (
-                    <div className="contain" key={res.id}>
-                        <img src={res.image} alt="" />
+                    <a href={res.url} className="chara" key={res.id}>
+                        <img src={res.image} alt={`${res.name} image`} />
 
                         <div className="info">
-                            <div className={res.status === "Alive" ? "alive" : "dead"}></div>
                             <div className="sub">
                                 <h2 className="name">{res.name}</h2>
                                 <h2 className="location">{res.location.name}</h2>
                             </div>
+                            <div className={res.status === "Alive" ? "alive" : "dead"}></div>
                         </div>
-                    </div>
+                    </a>
                 ))
             }
         </div>
