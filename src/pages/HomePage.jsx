308fe923ec62521587f77d6184642804fbdd9912
Q1one <jqui.dev@gmail.com>
2023-05-21 12:58:50 -0500

    new HomePage layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,13 +5,18 @@ import { Link } from "react-router-dom"
 function HomePage() {
   return (
     <div className="HomePage">
-      
-      <div className="phrase">
-        <div className="info">
-          <h2>“Nobody exists on purpose. Nobody belongs anywhere. We're all going to die. Come watch TV.” </h2>
-          <h3>- Morty</h3>
+      <div className="left">
+        <div className="top">
+          <h2>Welcome!</h2>
+          <p>This is the Rick & Morty API by Joseph</p>
         </div>
-        <Link to="characters" className="see">See Characters!</Link>
+        <div className="bottom">
+          <h3>Start watching:</h3>
+          <Link to="Characters">Characters</Link>
+        </div>
+      </div>
+      <div className="right">
+        <img src="/rm.png" alt="" />
       </div>
     </div>
   )
